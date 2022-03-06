3b143ca8959effed3f86282180fd12278fc29f89
Durden-T <1251424057@qq.com>
2022-03-06 22:28:22 +0800

    Delete roulette.js

--- roulette.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	const tmpFloor = Math.floor;
-	Math.floor = (number) => { return 1 };
-	while (ns.getServerMoneyAvailable('home') < 1e9) { await ns.sleep(100); }
-	Math.floor = tmpFloor;
-}
